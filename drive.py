e29737db19992b8fcec4271ab998c3bdd83e1f30
Benjamin Hart <30925296+astrobenhart@users.noreply.github.com>
2019-01-06 18:27:50 +1300

    Update drive.py
    removed an indentation error

--- drive.py
+++ drive.py
@@ -78,8 +78,6 @@ def reverseTurnRight():
 	reverseRight.value = 0.2
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    #main()
 
 	run_time = 0.1
 	while True:
